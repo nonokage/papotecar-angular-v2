8f3feb01f2befcd841b518227f2d3960e4ab03a7
nonokage <nononokage@gmail.com>
2018-04-28 10:38:48 +0200

    Integration des models - services + maj du app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { UserComponent } from './models/user/user.component';
+import { RunComponent } from './models/run/run.component';
+import { CommentComponent } from './models/comment/comment.component';
+import { CityComponent } from './models/city/city.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes } from '@angular/router';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    UserComponent,
+    RunComponent,
+    CommentComponent,
+    CityComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
